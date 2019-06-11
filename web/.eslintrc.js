e035f9c5594f3f54dab1b9bee898a2295736cb59
Marcin Miler <marcin4951@gmail.com>
2019-06-11 18:17:03 +0200

    Configure eslint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    extends: 'react-app'
+    extends: ['react-app', 'eslint:recommended', 'airbnb', 'prettier'],
+    rules: {
+        'react/jsx-filename-extension': 0,
+        semi: 0,
+        'import/no-unresolved': 0,
+        'react/jsx-indent': ['warning', 4],
+        'jsx-quotes': 0
+    }
 }
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import * as serviceWorker from './serviceWorker'
 import { Routes } from './Routes'
-import { ThemeProvider } from 'styled-components'
 import { theme, GlobalStyle } from './Theme'
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
         <>
             <GlobalStyle />
-            <React.Suspense fallback={'...Loading'}>
+            <React.Suspense fallback="...Loading">
                 <Routes />
             </React.Suspense>
         </>
