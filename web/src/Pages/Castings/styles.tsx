0fab643e10a56b91add550fbaaa4b02f9e25cd7e
Marcin Miler <marcin4951@gmail.com>
2019-06-16 19:20:37 +0200

    Styling casting items

--- web/src/Pages/Castings/styles.tsx
+++ web/src/Pages/Castings/styles.tsx
@@ -2,9 +2,14 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     width: 100%;
+    height: calc(100vh - 80px);
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1.2fr 1fr;
+    background-color: #f8f8f8;
 `
 export const CastingsWrapper = styled.div`
-    padding: 20px;
+    flex: 1;
+    margin-top: 80px;
+    padding: 10px 40px 0 40px;
+    overflow: auto;
 `
--- web/src/Theme/theme.ts
+++ web/src/Theme/theme.ts
@@ -11,9 +11,7 @@ interface Theme {
 export const theme: Theme = {
     name: 'Deafult',
     colors: {
-        darkblue: '#21346A',
-        lightgray: '#F1F1F6',
-        darkgray: '#C9C9C9'
+        lightgray: '#5A6371'
     },
     fontSize: {
         extraSmall: '0.8rem',
