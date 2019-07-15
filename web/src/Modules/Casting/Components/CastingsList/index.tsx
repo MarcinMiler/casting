e9a8b1c9e22fdae1ed8b75817ff41e0c9236e9a6
Marcin Miler <marcin4951@gmail.com>
2019-07-15 11:17:25 +0200

    Infinite scroll with virtualizing

--- web/src/Modules/Casting/Components/CastingsList/index.tsx
+++ web/src/Modules/Casting/Components/CastingsList/index.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react'
+import { AutoSizer, List, InfiniteLoader } from 'react-virtualized'
+
 import { getMoreCastingsAsync } from 'Modules/Casting/actions'
 import { CastingsQuery_castings } from 'GraphqlTypes'
 import { Casting } from '../CastingItem'
 import { CastingsWrapper } from './style'
+
+interface RowRendererParams {
+    index: number
+    style: object
+}
+
+interface RowLoadedParams {
+    index: number
+}
 
 interface Props {
     castings: CastingsQuery_castings[]
@@ -13,29 +24,51 @@ export const CastingsList: React.FC<Props> = ({
     castings,
     getMoreCastings
 }) => {
-    const containerRef = React.useRef<HTMLDivElement>(null)
+    const loadMoreRows = () => {
+        getMoreCastings({
+            cursor: castings[castings.length - 1].createdAt
+        })
 
-    const handleScroll = (e: any) => {
-        if (containerRef.current) {
-            const {
-                scrollTop,
-                scrollHeight,
-                offsetHeight
-            } = containerRef.current
-
-            if (scrollTop >= scrollHeight - offsetHeight - 200) {
-                getMoreCastings({
-                    cursor: castings[castings.length - 1].createdAt
-                })
-            }
-        }
+        return Promise.resolve()
     }
 
+    const isRowLoaded = ({ index }: RowLoadedParams) => {
+        if (castings[index]) {
+            return true
+        }
+
+        return false
+    }
+
+    const Castings = ({ style, index }: RowRendererParams) => (
+        <div style={style} key={castings[index].id}>
+            <Casting casting={castings[index]} />
+        </div>
+    )
+
     return (
-        <CastingsWrapper ref={containerRef} onScroll={e => handleScroll(e)}>
-            {castings.map(casting => (
-                <Casting key={casting.id} casting={casting} />
-            ))}
+        <CastingsWrapper>
+            <AutoSizer>
+                {({ width, height }) => (
+                    <InfiniteLoader
+                        rowCount={1000} // <- I need make request for total castings length
+                        loadMoreRows={loadMoreRows}
+                        isRowLoaded={isRowLoaded}
+                    >
+                        {({ onRowsRendered, registerChild }) => (
+                            <List
+                                width={width}
+                                height={height}
+                                onRowsRendered={onRowsRendered}
+                                ref={registerChild}
+                                rowCount={castings.length}
+                                rowHeight={160}
+                                rowRenderer={Castings}
+                            />
+                        )}
+                    </InfiniteLoader>
+                )}
+            </AutoSizer>
         </CastingsWrapper>
     )
 }
